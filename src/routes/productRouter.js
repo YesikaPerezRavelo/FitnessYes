f8c77a948857bd236da30f77e6d62a41d456bf52
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-06-10 22:12:22 -0300

    errors

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import CustomError from "../services/errors/CustomError.js";
+import { generateProductsErrorInfo } from "../services/errors/info.js";
+import { ErrorCodes } from "../services/errors/enums.js";
 import ProductController from "../controllers/productController.js";
 import { uploader } from "../utils/multerUtil.js";
-import productModel from "../models/productModel.js";
 import passport from "passport";
 import { auth } from "../middlewares/auth.js";
+import productModel from "../models/productModel.js";
 
 const router = Router();
 const productController = new ProductController();
@@ -11,10 +14,10 @@ const productController = new ProductController();
 router.get("/", async (req, res) => {
   try {
     let { limit, page, query, sort } = req.query;
-    query = query ? JSON.parse(query) : {}; // Assuming query is passed as a JSON string
+    query = query ? JSON.parse(query) : {};
     limit = limit ? parseInt(limit) : undefined;
     page = page ? parseInt(page) : undefined;
-    sort = sort ? JSON.parse(sort) : undefined; // Assuming sort is passed as a JSON string
+    sort = sort ? JSON.parse(sort) : undefined;
 
     const result = await productController.getAllProducts(
       limit,
@@ -35,6 +38,34 @@ router.get("/", async (req, res) => {
   }
 });
 
+// router.post(
+//   "/",
+//   passport.authenticate("jwt", { session: false }),
+//   auth("teacher"),
+//   uploader.array("thumbnails", 3),
+//   async (req, res) => {
+//     if (req.files) {
+//       req.body.thumbnails = [];
+//       req.files.forEach((file) => {
+//         req.body.thumbnails.push(file.filename);
+//       });
+//     }
+
+//     try {
+//       const result = await productController.createProduct(req.body);
+//       res.send({
+//         status: "success",
+//         payload: result,
+//       });
+//     } catch (error) {
+//       res.status(400).send({
+//         status: "error",
+//         message: error.message,
+//       });
+//     }
+//   }
+// );
+
 router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
@@ -42,9 +73,36 @@ router.post(
   uploader.array("thumbnails", 3),
   async (req, res) => {
     if (req.files) {
-      req.body.thumbnails = [];
-      req.files.forEach((file) => {
-        req.body.thumbnails.push(file.filename);
+      req.body.thumbnails = req.files.map((file) => file.filename);
+    }
+
+    const { title, description, code, price, status, stock, category } =
+      req.body;
+
+    if (
+      !title ||
+      !description ||
+      !code ||
+      !price ||
+      typeof status === "undefined" ||
+      !stock ||
+      !category
+    ) {
+      const product = {
+        title,
+        description,
+        code,
+        price,
+        status,
+        stock,
+        category,
+      };
+      const errorInfo = generateProductsErrorInfo(product);
+      return CustomError.createError({
+        name: "Product creation error",
+        cause: errorInfo,
+        message: "Error trying to create Product",
+        code: ErrorCodes.INVALID_TYPES_ERROR,
       });
     }
 
@@ -65,10 +123,7 @@ router.post(
 
 router.put("/:pid", uploader.array("thumbnails", 3), async (req, res) => {
   if (req.files) {
-    req.body.thumbnails = [];
-    req.files.forEach((file) => {
-      req.body.thumbnails.push(file.filename);
-    });
+    req.body.thumbnails = req.files.map((file) => file.filename);
   }
 
   try {
@@ -106,7 +161,6 @@ router.delete("/:pid", async (req, res) => {
 router.get("/search", async (req, res) => {
   try {
     const { title } = req.query;
-    console.log(req.query);
     let query = {};
     if (title) query = { title };
 
