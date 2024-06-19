c9766a8eb468445f92fb441d96838b3ca1646d47
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-06-19 08:56:03 -0300

    ruta logger

--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -200,15 +200,15 @@ router.get("/mockingproducts", (req, res) => {
   });
 });
 
-// router.get("/loggertest", (req, res) => {
-//   req.logger.fatal("Logger test fatal message");
-//   req.logger.error("Logger test error message");
-//   req.logger.warning("Logger test warning message");
-//   req.logger.info("Logger test info message");
-//   req.logger.http("Logger test http message");
-//   req.logger.debug("Logger test debug message");
+router.get("/loggertest", (req, res) => {
+  req.logger.fatal("Logger test fatal message");
+  req.logger.error("Logger test error message");
+  req.logger.warning("Logger test warning message");
+  req.logger.info("Logger test info message");
+  req.logger.http("Logger test http message");
+  req.logger.debug("Logger test debug message");
 
-//   res.send("Logger test completed!");
-// });
+  res.send("Logger test completed!");
+});
 
 export default router;
