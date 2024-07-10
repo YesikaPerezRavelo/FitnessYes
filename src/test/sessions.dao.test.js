956d53b3521c38671c3218c098441e8e25b7fefd
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-07-09 23:54:29 -0300

    testing

--- src/test/sessions.dao.test.js
+++ src/test/sessions.dao.test.js
@@ -9,6 +9,12 @@ const uri = process.env.URI;
 mongoose.connect(uri, { dbName: "testing" });
 const dao = new UserDao();
 const assert = Assert.strict;
+const testUser = {
+  firstName: "Testing",
+  lastName: "Fitness",
+  email: "testing@gmail.com",
+  password: "test123456",
+};
 
 // This groups all the related tests together
 describe("Tests DAO Users", function () {
@@ -16,16 +22,28 @@ describe("Tests DAO Users", function () {
   before(function () {});
 
   // Runs before each individual test. Use it to reset things so each test starts fresh.
-  beforeEach(function () {});
+  beforeEach(function () {
+    this.timeout(3000); // Set timeout to 3 seconds
+  });
 
   // Runs once after all tests finish. Use it to clean up things set up in before.
-  after(function () {});
+  after(function () {
+    // mongoose.disconnect();
+  });
 
   // Runs after each individual test. Use it to clean up after each test.
   afterEach(function () {});
 
   // This is a single test.
   it("getAll() should return all users", async function () {
-    // Add test logic here
+    const result = await dao.getAll();
+    assert.strictEqual(Array.isArray(result), true);
+    console.log(result);
+  });
+
+  it("create() should save my new user", async function () {
+    const result = await dao.create(testUser);
+    assert.strictEqual(typeof result, "object");
+    assert.ok(result._id);
   });
 });
