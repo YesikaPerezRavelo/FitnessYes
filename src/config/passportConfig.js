db30c873b1241a917589ff02ed8cfc1aa0830fad
Yesika Perez Ravelo <yesikapr@gmail.com>
2024-08-14 13:09:03 -0300

    github functioning

--- src/config/passportConfig.js
+++ src/config/passportConfig.js
@@ -52,16 +52,15 @@ const initializePassport = () => {
       },
       async (_accessToken, _refreshToken, profile, done) => {
         try {
-          const email = profile._json.email || profile.profileUrl; // Handle cases where email might be null
-          console.log("GitHub Profile:", profile);
+          const email =
+            profile._json.email || `${profile._json.login}@github.com`;
+          const user = await userModel.findOne({ email }); // Handle cases where email might be null
 
-          let user = await userModel.findOne({ email });
           console.log("Existing User:", user);
 
           if (!user) {
             const newUser = {
               id: profile.id,
-              username: profile._json.login,
               firstName: profile._json.name,
               email: email,
               role: "student",
@@ -105,111 +104,3 @@ const initializePassport = () => {
 };
 
 export default initializePassport;
-
-// import passport from "passport";
-// import GitHubStrategy from "passport-github2";
-// import jwt, { ExtractJwt } from "passport-jwt";
-// import { userModel } from "../models/userModel.js";
-// import userController from "../controllers/userController.js";
-// import cartController from "../repository/cartRepository.js";
-// import * as dotenv from "dotenv";
-// dotenv.config();
-
-// const userControllerDB = new userController();
-// const cartControllerDB = new cartController();
-
-// const JWTStrategy = jwt.Strategy;
-
-// const cookieExtractor = (req) => {
-//   let token = null;
-//   if (req && req.cookies) {
-//     token = req.cookies.auth ?? null;
-//   }
-//   return token;
-// };
-
-// const initializePassport = () => {
-//   const Client_Id = process.env.CLIENT_ID;
-//   const Secret_Id = process.env.SECRET_ID;
-//   const secretKey = process.env.SECRET_KEY;
-
-//   passport.use(
-//     "jwt",
-//     new JWTStrategy(
-//       {
-//         jwtFromRequest: ExtractJwt.fromExtractors([cookieExtractor]),
-//         secretOrKey: secretKey,
-//       },
-//       async (jwt_payload, done) => {
-//         try {
-//           const user = await userModel.findById(jwt_payload._id);
-//           if (user) {
-//             return done(null, user);
-//           } else {
-//             return done(null, false);
-//           }
-//         } catch (error) {
-//           return done(error);
-//         }
-//       }
-//     )
-//   );
-
-//   passport.use(
-//     "github",
-//     new GitHubStrategy(
-//       {
-//         clientID: Client_Id,
-//         clientSecret: Secret_Id,
-//         callbackURL: "http://localhost:8080/api/session/githubcallback",
-//       },
-//       async (_accessToken, _refreshToken, profile, done) => {
-//         try {
-//           const email =
-//             profile._json.email || `${profile._json.login}@github.com`;
-//           const user = await userModel.findOne({ email });
-
-//           if (!user) {
-//             const newUser = {
-//               id: profile.id,
-//               username: profile._json.login,
-//               firstName: profile._json.name,
-//               email: email,
-//               role: "student",
-//               password: "temporaryPassword", // Placeholder, you might need to handle this better
-//             };
-
-//             const registeredUser = await userControllerDB.registerUser(newUser);
-//             const cart = await cartControllerDB.createCart(registeredUser._id);
-//             registeredUser.cartId = cart._id;
-//             await registeredUser.save();
-//             return done(null, registeredUser);
-//           } else {
-//             if (user.cartId) {
-//               await cartControllerDB.updateCart(user.cartId, { products: [] });
-//             }
-//             return done(null, user);
-//           }
-//         } catch (error) {
-//           console.error("Error in GitHub Strategy:", error);
-//           return done(error);
-//         }
-//       }
-//     )
-//   );
-
-//   passport.serializeUser((user, done) => {
-//     done(null, user._id);
-//   });
-
-//   passport.deserializeUser(async (id, done) => {
-//     try {
-//       const user = await userModel.findById(id);
-//       done(null, user);
-//     } catch (error) {
-//       done(error);
-//     }
-//   });
-// };
-
-// export default initializePassport;
